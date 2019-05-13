89e0189d46b53fe1716fdbbdf6b0f481a351a9f2
NeivisFrancoReyes <50598520+NeivisFrancoReyes@users.noreply.github.com>
2019-05-13 16:01:21 -0500

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo3AHerencia.clases
+{
+    public abstract class Animal
+    {
+
+        public abstract  string hacerRuido();
+
+        public virtual  void comer()
+        {
+
+        }
+
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        private int peso;
+
+        public int Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+        private int edad;
+
+        public Animal(string nombre, int peso, int edad)
+        {
+            Nombre = nombre;
+            Peso = peso;
+            Edad = edad;
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+
+
+    }
+}
